0d7edcce132d8b3c00861bca4608ec38d950db77
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-02-09 18:16:27 +0100

    update

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
-// Endpoint of the api
+// Endpoint of the api-----------------------------------------------------------
 const endpoint = "https://striveschool-api.herokuapp.com/books";
 
+// Global variables--------------------------------------------------------------
 let searchInput = document.getElementById("searchInput");
 
-searchInput.addEventListener("input", () => {
-  fetch(`${endpoint}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
-});
+let bookList = document.getElementById("book-list");
 
+let cartList = document.getElementById("cart-list");
+
+let ShowBooks = (data) => {
+    data.forEach(item => {
+        let card = document.createElement('div');
+        card.classList.add("col-lg-3", "col-sm-4", "mb-4");
+        card.innerHTML = `
+                            <div class="card">
+                                <div>
+                                    <img src="${item.img}" alt="${item.title}" class="card-img-top rounded" width="25px" />
+                                </div>
+                                <div class="mt-1 mx-3">
+                                    <p>${item.title}</p>
+                                    
+                                </div>
+                                <div class="d-flex justify-content-between mx-4">
+                                    <p>${item.price}</p>
+                                    <div>
+                                        <i class="bi-star"></i>
+                                        <i class="bi bi-cart-plus"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            `;
+        // Aggiungi la card al contenitore dei risultati della ricerca
+        bookList.appendChild(card);
+    });
+
+    
+};
+
+// MOSTRAMI TUTTI I LIBRI--------------------------------------------------------------
+window.onload = () => {
+    fetch(`${endpoint}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            ShowBooks(data);
+        })
+        .catch((err) => console.log(err));
+};
+
+searchInput.addEventListener("input", () => {
+    // Pulisco il contenitore dei risultati
+    bookList.innerHTML = "";
+    
+    // Recupera il valore inserito nell'input di ricerca, eliminando eventuali spazi vuoti iniziali e finali
+    const value = searchInput.value.trim();
+    fetch(`${endpoint}`)
+        .then((res) => res.json())
+        .then((data) => {
+            // Filtra i dati in base alla parola cercata nell'input
+            const filteredData = data.filter(item => item.title.toLowerCase().includes(value.toLowerCase()));
+            
+            // Mostra solo i libri che corrispondono ai criteri di ricerca
+            ShowBooks(filteredData);
+        })
+        .catch((err) => console.log(err));
+});
